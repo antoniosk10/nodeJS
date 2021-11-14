04eb8370e303271a6db03067ad963871868fa7c5
antoniosk10 <antoniosk10@gmail.com>
2021-11-14 16:51:02 +0300

    feat/change readme

--- CLI_tool/stream/transformStream.js
+++ CLI_tool/stream/transformStream.js
@@ -1,17 +1,18 @@
 const transformString = require("../utils/transformString");
 const {Transform} = require("stream");
 const getValueByFlag = require("../utils/getValueByFlag");
+const {stdout} = require("process");
 
 class TransformStringStream extends Transform {
     _transform(chunk, encoding, callback) {
         try {
             const chunkString = chunk.toString("utf8");
             const transformSteps = getValueByFlag("config").split("-");
-            const result = transformSteps.reduce(
+            const transformedString = transformSteps.reduce(
                 (tempString, step) => transformString(tempString, step),
                 chunkString,
             );
-
+            const result = `Результат шифрования:\n${transformedString}`;
             callback(null, result);
         } catch (err) {
             callback(err);
